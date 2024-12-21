99018d39c027f23a50e11f9d970880d2854c5711
NeyThuAung <neythuaung.com>
2024-12-21 16:21:21 +0700

    Added Lint check  in  main.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.android_cicd_github_action"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.android_cicd_github_action"
